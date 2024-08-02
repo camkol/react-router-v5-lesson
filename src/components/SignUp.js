37a89a2f96d84aba7a1bfa97962b17fa35b1555b
camkol <koliyus3@hotmail.com>
2024-08-02 21:37:18 +0200

    implement useHistory

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,20 +1,22 @@
 import React, { useState } from "react";
-import { signUp } from "../features/session/sessionSlice"
+import { signUp } from "../features/session/sessionSlice";
 import { useDispatch } from "react-redux";
 // import useHistory
+import { useHistory } from "react-router-dom";
 
-export default function SignUp () {
+export default function SignUp() {
   const [username, setUsername] = useState("");
   const dispatch = useDispatch();
-  
+
   // Grab the history object
+  const history = useHistory();
 
-  const handleSubmit = e => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    dispatch(signUp({username: username}));
+    dispatch(signUp({ username: username }));
     // imperatively redirect the user to /profile
-
-  }
+    history.push("/profile");
+  };
 
   return (
     <section>
