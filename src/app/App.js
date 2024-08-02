92c4c75fc0d9913dd0073f0eed2c2227b0fcc74e
camkol <koliyus3@hotmail.com>
2024-08-02 11:37:57 +0200

    include switch, fix nested route

--- src/app/App.js
+++ src/app/App.js
@@ -8,7 +8,7 @@ import Article from "../components/Article";
 import Categories from "../components/Categories";
 import Author from "../components/Author";
 import Profile from "../components/Profile";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import "./App.css";
 
@@ -18,27 +18,30 @@ function App() {
       <Header />
       <main>
         {" "}
-        <Route path="/articles/:title">
-          <Article />{" "}
-        </Route>
-        <Route path="/author/:name">
-          <Author />{" "}
-        </Route>
-        <Route path="/about">
-          <About />
-        </Route>
-        <Route path="/sign-up">
-          <SignUp />{" "}
-        </Route>
-        <Route path="/articles">
-          <Articles />{" "}
-        </Route>
-        <Route path="/categories">
-          <Categories />{" "}
-        </Route>
-        <Route path="/profile">
-          <Profile />{" "}
-        </Route>
+        <Switch>
+          {" "}
+          <Route path="/articles/:title">
+            <Article />{" "}
+          </Route>
+          <Route path="/author/:name">
+            <Author />{" "}
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/sign-up">
+            <SignUp />{" "}
+          </Route>
+          <Route path="/articles">
+            <Articles />{" "}
+          </Route>
+          <Route path="/categories">
+            <Categories />{" "}
+          </Route>
+          <Route path="/profile">
+            <Profile />{" "}
+          </Route>
+        </Switch>
       </main>
       <Footer />
     </Router>
